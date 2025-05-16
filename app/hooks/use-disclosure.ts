62ccb457bde0321dbd94a7e3c113533defed60c8
zipang <christophe.desguez@gmail.com>
2025-05-16 22:29:00 +0200

    Some fancy hooks

---
+++ app/hooks/use-disclosure.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+type eventHandler = (() => void) | undefined;
+
+export const useDisclosure = (
+	initialState = false,
+	onOpen: eventHandler,
+	onClose: eventHandler,
+) => {
+	const [isOpen, setIsOpen] = useState(initialState);
+
+	const open = () => {
+		setIsOpen(true);
+		if (typeof onOpen === "function") {
+			onOpen();
+		}
+	};
+
+	const close = () => {
+		setIsOpen(false);
+		if (typeof onClose === "function") {
+			onClose();
+		}
+	};
+
+	const toggle = () => (isOpen ? close() : open());
+
+	return { isOpen, open, close, toggle };
+};
