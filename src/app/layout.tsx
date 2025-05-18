0b41005720c66cc12c81708e976b1641f54bc9d8
zipang <christophe.desguez@gmail.com>
2025-05-18 22:07:18 +0200

    fix a trailing char preventing good rehydration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import type { FC, PropsWithChildren } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme";
-import type { FC, PropsWithChildren } from "react";
 
 const RootLayout: FC<PropsWithChildren> = ({ children }) => (
 	<html lang="en">
@@ -10,7 +9,7 @@ const RootLayout: FC<PropsWithChildren> = ({ children }) => (
 				href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700;900&display=swap"
 				rel="stylesheet"
 			/>
-			<link rel="icon" href="/movie-icon.svg" />"
+			<link rel="icon" href="/movie-icon.svg" />
 		</head>
 		<body>
 			<ChakraProvider value={theme}>{children}</ChakraProvider>
