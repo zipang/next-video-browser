8dca9c3cbc09f1528f8685897da9c95ddb15f7ce
zipang <christophe.desguez@gmail.com>
2025-05-19 12:58:28 +0200

    Improve documentation on returned type

--- src/app/hooks/use-disclosure.ts
+++ src/app/hooks/use-disclosure.ts
@@ -1,29 +1,55 @@
 import { useState } from "react";
 
 type eventHandler = () => void;
+/**
+ * toggle the state
+ */
+type toggleDisclosure = () => void;
+/**
+ * transition to open
+ */
+type openDisclosure = () => void;
+/**
+ * transition to close
+ */
+type closeDisclosure = () => void;
+
+/**
+ * Tuple that allows to easily name each returned elements
+ * @example: const [isPanelOpen, togglePanel, openPanel, closePanel] = useDisclosure({ defaultOpen: true })
+ */
+type UseDisclosureReturn = [
+	/**
+	 * The current open/close state
+	 */
+	boolean,
+	toggleDisclosure,
+	openDisclosure,
+	closeDisclosure
+];
 
 /**
  * Open/close an UI element and provide means to listen to it
  */
 export const useDisclosure = ({
-	initialState = false,
+	defaultOpen = false,
 	onOpen,
 	onClose
 }: {
 	/**
 	 * Should the element be initially visible (TRUE) or hidden (FALSE as default)
 	 */
-	initialState: boolean;
+	defaultOpen: boolean;
 	/**
-	 * Optional callback triggered whenever the object is disclosed
+	 * Optional callback triggered whenever the element is disclosed
 	 */
 	onOpen?: eventHandler;
 	/**
-	 * Optional callback triggered whenever the object is closed
+	 * Optional callback triggered whenever the element is closed
 	 */
 	onClose?: eventHandler;
 }) => {
-	const [isOpen, setIsOpen] = useState(initialState);
+	const [isOpen, setIsOpen] = useState(defaultOpen);
 
 	const open = () => {
 		setIsOpen(true);
@@ -42,5 +68,5 @@ export const useDisclosure = ({
 	const toggle = () => (isOpen ? close() : open());
 
 	// Elements are returned in that order in a tuple so that they can easily be renamed
-	return [isOpen, toggle, open, close];
+	return [isOpen, toggle, open, close] as UseDisclosureReturn;
 };
