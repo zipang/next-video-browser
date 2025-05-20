bd58b8b83a68ac7bed962d8385d6183ca108e027
zipang <christophe.desguez@.com>
2025-05-20 19:11:23 +0200

    Reorganize codebase (move components/ and hooks/ dir to the root)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,15 @@
 "use client";
 
-import { PlayerStateProvider, type Video } from "./components/PlayerStateProvider";
-import { VideoGallery } from "./components/VideoGallery";
+import { PlayerStateProvider, type Video } from "@components/PlayerStateProvider";
+import { VideoGallery } from "@components/VideoGallery";
 
 // Sample video data (could be result of external API call)
 import videos from "./videos.json";
 
-const Page = () => (
+const HomePage = () => (
 	<PlayerStateProvider playlist={videos}>
 		<VideoGallery videos={videos} />
 	</PlayerStateProvider>
 );
 
-export default function Home() {
-	return (
-		<main>
-			<Page />
-		</main>
-	);
-}
+export default HomePage;
